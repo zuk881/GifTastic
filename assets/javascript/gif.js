086ecd19caf85c42142fb39b9f926e94f07a7af4
zuk881 <zuk881@cox.net>
2020-01-15 21:29:40 -0700

    Finished javascript, added styling and comments

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,39 +1,61 @@
 
-
+// array to hold data values, button text, and search parameters
 var giphy = ["silly cat", "happy cat", "scared cat", "tired cat"];
 
-
+// function used to get data from giphy.com and display the gifs
 function displayGif() {
+  
+  // call to remove previous gifs before adding new ones
   $("#gifs-appear-here").empty();
+
+  // variable to hold the data of button pushed that is then added to search
   var catType = $(this).attr("data-name");
 
+  // variable to hold the parameters of ajax call
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + catType + "&api_key=94tWcGFM6E7Wgp6J65im7ukVX5wN4Z3q&limit=10";
 
+  // ajax call to get data from giphy.com
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
 
-    console.log(response)
+    // for loop used to disply all results from query  
     for (var i = 0; i < 10; i++) {
+
+      // variables needed to hold values
       var gifDiv = $("<div>");
       var results = response.data;
       var rating = results[i].rating;
-
       var p = $("<p>").text("Rating: " + rating);
-
       var catImage = $("<img>");
-      catImage.attr("src", results[i].images.fixed_height.url);
 
+      // adding attributes needed for still/animate function and adding images and ratings to gifDiv and then displaying them when called    
+      catImage.attr("src", results[i].images.fixed_height.url);
+      catImage.attr("data-still", results[i].images.fixed_height_still.url);
+      catImage.attr("data-animate", results[i].images.fixed_height.url);
+      catImage.addClass("gif")
       gifDiv.prepend(p);
       gifDiv.prepend(catImage);
-
       $("#gifs-appear-here").prepend(gifDiv);
-console.log(catType);
+
+      // click on gif listener to change data-state from active to still and reverse    
+      $(".gif").on("click", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+
     }
   });
 }
 
+// function to loop through the array and display buttons
 function renderButtons() {
   $("#buttons-view").empty();
   for (var i = 0; i < giphy.length; i++) {
@@ -45,11 +67,20 @@ function renderButtons() {
   }
 }
 
+// on-click function to get value out of submit form, push into the array, and display new button
 $("#add-cat").on("click", function (event) {
   event.preventDefault();
   var giph = $("#gif-input").val().trim();
   giphy.push(giph);
   renderButtons();
+
 });
+
+// button event listener to display 10 gifs 
 $(document).on("click", ".gif-button", displayGif);
+
+// function call to display initial buttons  
 renderButtons();
+
+
+
